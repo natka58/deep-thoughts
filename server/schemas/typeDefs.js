67dbdc8c25d3f4a2c41d9b5f786c179ed0dda5e4
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-06 18:35:57 -0700

    21.3.6

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,24 +3,6 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-
-
-type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
-  
   type User {
     _id: ID
     username: String
@@ -29,29 +11,41 @@ type Thought {
     thoughts: [Thought]
     friends: [User]
   }
-
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
   type Query {
+    me: User
     users: [User]
-  user(username: String!): User
-  thoughts(username: String): [Thought]
-  thought(_id: ID!): Thought
- }
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+ type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addThought(thoughtText: String!): Thought
+  addReaction(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
+}
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // export the typeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -9,11 +10,13 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { authMiddleware } = require('./utils/auth');
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -21,6 +24,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
